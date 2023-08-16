85423db5572413126c474217da85c4f1cbcf369e
Talin Sharma <talinsharma.dev@gmail.com>
2023-08-16 14:22:07 -0400

    Did git config stuff

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Talin's First NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      TalinsNix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.talin = import ./home.nix;
+
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "talin";
+  home.homeDirectory = "/home/talin";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 101;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Talin Sharma";
+    userEmail = "talinsharma.dev@gmail.com";
+    extraConfig = {
+      safe.directory = "/etc/nixos";
+    };
+  };
+
+  home.packages = with pkgs; [
+    neofetch
+    vscode
+    google-chrome
+    fish
+
+    zip
+    xz
+    unzip
+    p7zip
+
+    exa
+
+    file
+    which
+    tree
+
+    nix-output-monitor
+  ];
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = false;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+    };
+  };
+
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+}
