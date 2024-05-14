89c234bc808e631cde779b0ce5adbcb127c2928e
Talin Sharma <talinsharma.dev@gmai.com>
2024-05-14 18:29:55 -0400

    Nvim config file structure

---
+++ nvim/lua/keybinds.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_set_keymap("", "<leader>f", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
---
+++ nvim/lua/setup.lua
@@ -0,0 +1,16 @@
+-- Basic Settings --
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.number = true -- Line numbers
+
+vim.opt.undofile = true -- Persistant undo history
+vim.opt.title = true -- Terminal title
+vim.g.mapleader = " " 
+vim.g.maplocalleader = "\\"
+vim.o.background = "dark"
+vim.opt.termguicolors = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
