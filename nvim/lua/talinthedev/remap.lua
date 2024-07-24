1abfa49cbf120eac531cf297a0617a267c99d16a
Talin Sharma <talinsharma.dev@gmai.com>
2024-07-24 19:34:54 -0400

    Added window quit remap

--- nvim/lua/talinthedev/remap.lua
+++ nvim/lua/talinthedev/remap.lua
@@ -3,7 +3,7 @@ vim.g.localmapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file tree" })
 vim.keymap.set("n", "<leader>ds", ":update<cr>", { desc = "[D]ocument [S]ave" })
-
+vim.keymap.set("n", "<leader>dq", ":q<cr>", { desc = "Window ([d]ocument) [Q]uit" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Stop highlighting search results on Esc" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
