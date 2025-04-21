5061d74f6172fe0287bd5b98938c4a6683aaea46
Talin Sharma <talinsharma.dev@gmail.com>
2025-04-20 22:17:07 -0400

    Removed tab bar on wezterm if only one tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,12 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
 
 -- Font
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font("JetBrains Mono")
 
 -- Color Scheme
-config.color_scheme = 'Black Metal (Gorgoroth) (base16)'
+config.color_scheme = "Black Metal (Gorgoroth) (base16)"
+
+-- Misc.
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
