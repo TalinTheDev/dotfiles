c1cdb2295eb4d53e3323f9de6c3b3f157fc77afa
Talin Sharma <talinsharma.dev@gmail.com>
2025-04-20 22:35:45 -0400

    Added git abbr to fish

---
+++ fish/.config/fish/conf.d/gitAbbrs.fish
@@ -0,0 +1,8 @@
+abbr -a --set-cursor='%' -- ga 'git add %'
+abbr -a -- gs 'git status'
+abbr -a -- gp 'git push'
+abbr -a -- gsa 'git status && git add'
+abbr -a --set-cursor='%' -- gas 'git add %  && git status'
+abbr -a --set-cursor='%' -- gacp "git add . && git commit -m '%' && git push"
+abbr -a --set-cursor='%' -- gac "git add . && git commit -m '%'"
+abbr -a --set-cursor='%' -- gc "git commit -m '%'"
