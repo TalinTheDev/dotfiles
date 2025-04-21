003dd1b8671f59c572606b0c7e2bb8b8f112a978
Talin Sharma <talinsharma.dev@gmail.com>
2025-04-21 01:59:55 -0400

    Telescope is still being annoying but im too tired to care

--- nvim/.config/nvim/lua/talinthedev/plugins/telescope.lua
+++ nvim/.config/nvim/lua/talinthedev/plugins/telescope.lua
@@ -35,21 +35,9 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set(
-			"n",
-			"<leader>sf",
-			-- builtin.find_files({ cwd = vim.loop.cwd(), hidden = true, no_ignore = false }),
-			builtin.find_files,
-			{ desc = "[S]earch [F]iles" }
-		)
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>st", builtin.builtin, { desc = "[S]earch [t]elescope builtins" })
-		vim.keymap.set(
-			"n",
-			"<leader>sw",
-			-- builtin.grep_string({ cwd = vim.loop.cwd() }),
-			builtin.grep_string,
-			{ desc = "[S]earch current [W]ord" }
-		)
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
