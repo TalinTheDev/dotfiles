92950c803e77057464c0bda5966f30a4a07bb684
Talin Sharma <talinsharma.dev@gmail.com>
2025-05-25 23:14:28 -0400

    Changed lsp.lua and treesitter.lua to use nvim-setup.lua when it exists

--- nvim/lua/talinthedev/plugins/lsp.lua
+++ nvim/lua/talinthedev/plugins/lsp.lua
@@ -27,6 +27,12 @@ return {
 			"saghen/blink.cmp",
 		},
 		config = function()
+			local proj_conf = {}
+			local ok, cfg = pcall(dofile, vim.fn.getcwd() .. "/nvim-setup.lua")
+			if ok then
+				proj_conf = cfg
+			end
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 				callback = function(event)
@@ -151,39 +157,40 @@ return {
 
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			local servers = {
-				clangd = {}, -- C++/C
-				rust_analyzer = {},
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							diagnostics = {
-								globals = { "vim" },
+			local servers = proj_conf.lsp
+				or {
+					clangd = {}, -- C++/C
+					rust_analyzer = {},
+					lua_ls = {
+						settings = {
+							Lua = {
+								completion = {
+									callSnippet = "Replace",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
 							},
 						},
 					},
-				},
-				astro = {}, -- Astro.js
-				bashls = {},
-				cssls = {},
-				gopls = {}, -- Golang
-				html = {},
-				eslint = {}, -- Javascript
-				jsonls = {},
-				markdown_oxide = {},
-				ruff = {}, -- Python linter in rust
-				sqlls = {},
-				taplo = {}, -- TOML
-				volar = {}, -- Vue.js
-				yamlls = {}, -- YAML
-				zls, -- Zig
-			}
+					astro = {}, -- Astro.js
+					bashls = {},
+					cssls = {},
+					gopls = {}, -- Golang
+					html = {},
+					eslint = {}, -- Javascript
+					jsonls = {},
+					markdown_oxide = {},
+					ruff = {}, -- Python linter in rust
+					sqlls = {},
+					taplo = {}, -- TOML
+					volar = {}, -- Vue.js
+					yamlls = {}, -- YAML
+					zls, -- Zig
+				}
 
 			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
+			vim.list_extend(ensure_installed, proj_conf.mason or {
 				-- Formatters
 				"stylua",
 				"beautysh", -- Bash
